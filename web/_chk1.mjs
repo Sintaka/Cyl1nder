88a9c314f8b05a95175d8ecc115bc75e6036ce70
Sintaka <1274174087@qq.com>
2026-08-09 23:17:38 +0800

    chore(web): ignore playwright results/vite logs/scratch files

--- web/_chk1.mjs
+++
@@ -1,6 +0,0 @@
-﻿import fs from "fs";
-const tv = JSON.parse(fs.readFileSync("node_modules/three/package.json", "utf8")).version;
-console.log("three version:", tv);
-const src = fs.readFileSync("node_modules/three/examples/jsm/controls/TransformControls.js", "utf8");
-console.log("TransformControls class line:", src.split("\n").find((l) => l.includes("class TransformControls")).trim());
-console.log("extends Object3D:", /class TransformControls extends Object3D/.test(src));
--- web/_chk2.mjs
+++
@@ -1,8 +0,0 @@
-﻿import { Node } from "@antv/x6";
-const C = Node.define({
-  name: "cyl1nder",
-  markup: [{ tagName: "rect", selector: "body" }],
-  attrs: { body: { width: 100, height: 40, fill: "#333" } },
-});
-console.log("registry.exist:", Node.registry.exist("cyl1nder"));
-console.log("registry.get:", !!Node.registry.get("cyl1nder"));
